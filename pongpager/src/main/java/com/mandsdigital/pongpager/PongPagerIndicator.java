a3e227deed075d97f08ce1173a5fd32a68ac859c
jacksonmiller0900 <andrewwang0900@gmail.com>
2016-07-01 13:28:09 +0100

    Initial commit. Created viewpager without the indicator.

---
+++ pongpager/src/main/java/com/mandsdigital/pongpager/PongPagerIndicator.java
@@ -0,0 +1,21 @@
+package com.mandsdigital.pongpager;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+
+public class PongPagerIndicator extends FrameLayout {
+
+    public PongPagerIndicator(Context context) {
+        this(context, null);
+    }
+
+    public PongPagerIndicator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+
+}
---
+++ sample/src/main/java/com/mandsdigital/pongpager/sample/SampleActivity.java
@@ -0,0 +1,60 @@
+package com.mandsdigital.pongpager.sample;
+
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+
+public class SampleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        int[] logos = {R.drawable.nougat_logo, R.drawable.marshmallow_logo, R.drawable.lollipop_logo, R.drawable.kitkat_logo};
+        SampleAdapter adapter = new SampleAdapter(logos);
+        viewPager.setAdapter(adapter);
+    }
+
+    class SampleAdapter extends PagerAdapter {
+
+        private int[] imageResources;
+
+        public SampleAdapter(int[] imageResources){
+            this.imageResources = imageResources;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            View itemView = LayoutInflater.from(container.getContext()).inflate(R.layout.viewpager_item, container, false);
+            ImageView image = (ImageView) itemView.findViewById(R.id.image);
+            Glide.with(container.getContext()).load(imageResources[position]).centerCrop().into(image);
+            container.addView(itemView);
+            return itemView;
+        }
+
+        @Override
+        public int getCount() {
+            return imageResources.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+            object = null;
+        }
+    }
+}
